4ebbfcf27f75608007e0d643771f053cee5d8223
gabriel Carino <gabejcarino@gmail.com>
2022-04-04 11:45:43 -0500

    commit tree 1afec92e5ee307b80629a4a2eebdde1133fb2fc8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, match) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === match.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, match) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, match.length) === match.toLowerCase()
+    })
+}
+
+function matchName(drivers, match) {
+    return drivers.filter(function (driver) {
+        return driver.name === match
+    })
+}
